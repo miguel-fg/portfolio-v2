61226c7e187ccb5f7934c56b39a4efb864c8cd79
miguel-fg <miguelfierro55@gmail.com>
2024-03-14 00:08:07 -0700

    [#28] fix project thumbnails
    Moved the thumbnail folder to the public folder instead of using dynamic imports

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,40 +1,16 @@
-import { useState, useEffect, Suspense } from "react";
 import { Link } from "react-router-dom";
 
 const ProjectItem = (props) => {
     const { name, subtitle, description, thumbnail } = props.project;
-    const [imgSrc, setImgSrc] = useState(null);
-
-    useEffect(() => {
-        const importImage = async() => {
-            try{
-                const module = await import(thumbnail);
-                setImgSrc(module.default);
-            } catch(error){
-                console.error("Error loading image: ", error);
-            }
-        };
-
-        importImage();
-
-        return ()=> {
-            setImgSrc(null);
-        }
-
-    }, [thumbnail])
 
     return (
         <div className="col-span-12 flex flex-row">
             <div className="w-96 flex items-center justify-center">
-                <Suspense fallback={<div>Loading...</div>}>
-                    {imgSrc && (
-                        <img
-                            src={imgSrc}
-                            alt="Project thumbnail"
-                            className="max-w-96 rounded-3xl"
-                        />
-                    )}
-                </Suspense>
+                <img
+                    src={thumbnail}
+                    alt="Project thumbnail"
+                    className="max-w-96 rounded-3xl"
+                />
             </div>
             <div className="ml-10 flex flex-col justify-between">
                 <h2 className="text-h2 font-LS text-oxford-blue leading-none">
