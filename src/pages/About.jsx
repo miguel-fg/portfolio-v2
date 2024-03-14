c11af5aa8b5b44296f2354a427b20d2e67b68f95
miguel-fg <miguelfierro55@gmail.com>
2024-03-13 21:47:11 -0700

    [#20] add about page content
    Also updated contact page paragraph styling to match the about page paragraph under the maing heading

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,122 @@
 import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
 import Divider from "../components/Divider";
+import about from "../images/photos/about.jpg";
+
+import { Link } from "react-router-dom";
 
 const About = () => {
     return (
         <>
-            <h1 className="text-h1 font-LS text-oxford-blue col-span-12 text-center mt-10">
+            <h1 className="text-h1 font-LS text-oxford-blue col-span-12 text-center mt-10 leading-none">
                 About Me
             </h1>
+            <p className="col-span-8 col-start-3 text-2xl font-MT text-oxford-blue text-center">
+                Hi! I&apos;m Miguel, a developer based in Vancouver, BC. Aside
+                from coding, I have many different hobbies including reading,
+                swimming, hiking, drawing, drumming, and watching movies.
+            </p>
+            <Divider />
+            <div className="text-body font-MT col-span-6 text-oxford-blue">
+                <p>
+                    In 2021, I completed my Bachelor&apos;s degree in Digital
+                    Systems and Robotics Engineering at{" "}
+                    <Link
+                        to="https://tec.mx/en"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="font-bold underline"
+                    >
+                        ITESM
+                    </Link>
+                    . After which I decided to focus on Computer Science and
+                    enroll in a diploma at{" "}
+                    <Link
+                        to="https://langara.ca/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="font-bold underline"
+                    >
+                        Langara College
+                    </Link>
+                    .
+                </p>
+                <br />
+                <p>
+                    My professional journey has been dynamic. It began with a
+                    position as a Python instructor at{" "}
+                    <Link
+                        to="https://www.linkedin.com/company/grupoaerobot/about/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="font-bold underline"
+                    >
+                        Aerobot Academy
+                    </Link>
+                    , where I had the privilege of guiding 3 generations of
+                    students through the program and instill the love for
+                    technology and the responsibility that comes with learning
+                    it.
+                </p>
+                <br />
+                <p>
+                    Later, I ventured as a 3D printing lab coordinator for my
+                    university, ITESM. Here, I explored the intersection between
+                    technology and manufacturing, and applied automation
+                    techniques to streamline processes from design to
+                    production.
+                </p>
+                <br />
+                <p>
+                    I then held a software deveolpment position at{" "}
+                    <Link
+                        to="https://intesc.mx/quienes-somos/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="font-bold underline"
+                    >
+                        INTESC
+                    </Link>
+                    , where I wrote Micropython libraries for image and audio
+                    processing for the{" "}
+                    <Link
+                        to="https://intesc.mx/productos/tarjeta-de-desarrollo-ophyra/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="font-bold underline"
+                    >
+                        Ophyra
+                    </Link>{" "}
+                    development board.
+                </p>
+                <br />
+                <p>
+                    My decision to pivot towards software and web development
+                    brought me to Vancouver to pursue new opportunities in this
+                    field.
+                </p>
+                <br />
+                <p>
+                    Over the years, I have acquired a versatile skill set
+                    encompassing frontend technologies like HTML, CSS, Tailwind,
+                    React, and Vue; backend tools including Node, PHP, and
+                    Python; database management with MySQL and MongoDB; and
+                    RESTful API development with Express and FastAPI.
+                </p>
+            </div>
+            <div className="col-span-6 col-start-7 flex items-center justify-center">
+                <img
+                    src={about}
+                    alt=""
+                    className="rounded-3xl drop-shadow-lg"
+                />
+            </div>
             <Divider />
             <ContactForm title="let's connect" />
             <Divider />
             <Footer />
         </>
     );
-}
+};
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,10 @@ const Contact = () => {
             <h1 className="text-h1 font-LS text-oxford-blue col-span-12 text-center mt-10 leading-none">
                 Contact Me
             </h1>
-            <p className="col-span-8 col-start-3 font-MT text-body text-oxford-blue text-center">Reach out to me using the form below or the contact information provided at the bottom of the page. I look forward to hearing from you!</p>
+            <p className="col-span-8 col-start-3 font-MT text-2xl text-oxford-blue text-center">
+                Reach out to me using the form below or the contact information at the bottom of the page. You can expect a reply within 1-2 business days. I look forward to hearing
+                from you!
+            </p>
             <Divider />
             <ContactForm title="send me a message" />
             <Divider />
